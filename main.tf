733ac52aae1cb74bb81b104ab72330dcc90efd1f
Sebastian Poxhofer <secustor@users.noreply.github.com>
2021-02-21 12:54:07 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,29 @@
 terraform {
   required_providers {
+    docker = {
+      source  = "terraform-providers/docker"
+      version = "2.7.2"
+    }
     aws = {
       source  = "aws"
-      version = "3.0.0"
+      version = "2.7.0"
+    }
+    // falls back block name for source
+    azurerm = {
+      version = "=2.27.0"
+    }
+    invalid = {
+      source  = "//hashicorp/helm"
+      version = "1.2.4"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "1.2.4"
+    }
+    kubernetes = {
+      source  = "terraform.example.com/hashicorp/kubernetes"
+      version = ">= 1.0"
     }
   }
+  required_version = "= 0.13.0"
 }
